1bff02992ab257c4488b4201a77bd194b2c5b6bf
HBusiness-Group <216771493+HBusiness-Group@users.noreply.github.com>
2026-03-31 09:01:51 -0400

    Change layout runtime from 'force-dynamic' to 'edge'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
-export const dynamic = 'force-dynamic'
+export const runtime = 'edge'
 
 export const metadata: Metadata = {
   title: 'NOOON Caixa',
